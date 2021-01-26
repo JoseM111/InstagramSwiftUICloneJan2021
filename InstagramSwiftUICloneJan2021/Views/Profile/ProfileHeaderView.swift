5305dc4c88165fa41514816f8b474c09184818d8
Jose Martinez <alias_one11@outlook.com>
2021-01-26 23:09:05 +0100

    Refactoring...

--- InstagramSwiftUICloneJan2021/Views/Profile/ProfileHeaderView.swift
+++ InstagramSwiftUICloneJan2021/Views/Profile/ProfileHeaderView.swift
@@ -99,35 +99,3 @@ struct ProfileHeaderView_Previews: PreviewProvider {
     }
 }
 
-/// @━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-
-// import SwiftUI
-
-struct UserStatView: View {
-    /// MARK: - ™PROPERTIES™
-    ///™━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-    
-    //™━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━«
-    
-    ///™━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-    
-    // MARK: -∆  body •••••••••
-    var body: some View {
-        
-        VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: /*@START_MENU_TOKEN@*/nil/*@END_MENU_TOKEN@*/, content: {
-            
-            Text("2")
-                .font(.system(size: 15, weight: .semibold))
-            
-            Text("Followers")
-                .font(.system(size: 15))
-        })
-        // MARK: ||END__PARENT-VSTACK||
-        .frame(width: 80, alignment: .center)
-    }
-    // MARK: |||END OF: body|||
-}
-// MARK: END OF: UserStatView
-
-/// @━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-
